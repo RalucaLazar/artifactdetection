789d52f5eedc33fd3d3bdfc388532edcf9da2de5
Sechel Raluca <ralu.rodi@yahoo.com>
2018-03-10 16:56:11 +0200

    Add changes for binary to text conversion.

--- service/src/main/java/business/conversion/BinaryToTextConversion.java
+++ service/src/main/java/business/conversion/BinaryToTextConversion.java
@@ -6,34 +6,17 @@ import java.text.DecimalFormat;
 public class BinaryToTextConversion {
 
     public static void binaryToText(String pathToBinaryFile, String pathToTextFile) throws IOException {
-        PrintWriter writer = null;
-        DecimalFormat df = new DecimalFormat("#.######");
-        try {
-            writer = new PrintWriter(pathToTextFile, "UTF-8");
-        } catch (FileNotFoundException | UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        DataInputStream input = null;
-        try {
-            input = new DataInputStream(new FileInputStream(pathToBinaryFile));
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        assert input != null;
+        PrintWriter printWriter = new PrintWriter(new FileOutputStream(pathToTextFile));
+        DataInputStream input = new DataInputStream(new FileInputStream(pathToBinaryFile));
+
         while (input.available() > 0) {
             try {
-                double nr = input.readDouble();
-                String str = Double.toString(nr);
-                str = str.replace("E-", "");
-                str = str.replace("E", "");
-                assert writer != null;
-                writer.println(Double.valueOf(df.format(Double.valueOf(str))));
+                printWriter.println(Float.intBitsToFloat(Integer.reverseBytes(input.readInt())));
             } catch (EOFException e) {
                 break;
             }
         }
-        assert writer != null;
-        writer.close();
+        printWriter.close();
     }
 
     public static String changeFileExtension(String file) {
